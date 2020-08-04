5e5075fdedd9a0f267e70e1485219139e3620053
Anthony Helka <helkaanthony1997@gmail.com>
2020-08-04 09:39:06 -0400

    Added Avatar component.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ exports.Avatar = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -16,27 +18,33 @@ function _objectWithoutProperties(source, excluded) { if (source == null) return
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
 
 var Avatar = function Avatar(_ref) {
-  var _ref$src = _ref.src,
-      src = _ref$src === void 0 ? 'https://i.pinimg.com/236x/cd/da/4b/cdda4beceee4594f5bdc0cf505a73d8b--camille-lol-league-of-legends-camille.jpg' : _ref$src,
-      _ref$width = _ref.width,
-      width = _ref$width === void 0 ? '32px' : _ref$width,
-      _ref$height = _ref.height,
-      height = _ref$height === void 0 ? '32px' : _ref$height,
-      _ref$borderRadius = _ref.borderRadius,
-      borderRadius = _ref$borderRadius === void 0 ? '100px' : _ref$borderRadius,
-      _ref$status = _ref.status,
-      status = _ref$status === void 0 ? 'offline' : _ref$status,
-      rest = _objectWithoutProperties(_ref, ["src", "width", "height", "borderRadius", "status"]);
+  var src = _ref.src,
+      size = _ref.size,
+      radius = _ref.radius,
+      status = _ref.status,
+      rest = _objectWithoutProperties(_ref, ["src", "size", "radius", "status"]);
 
   return /*#__PURE__*/_react["default"].createElement("img", _extends({
     src: src,
     style: {
-      width: width,
-      height: height,
-      borderRadius: borderRadius,
+      width: size,
+      height: size,
+      borderRadius: radius,
       objectFit: 'cover'
     }
   }, rest));
 };
 
 exports.Avatar = Avatar;
+Avatar.defaultProps = {
+  src: 'https://i.pinimg.com/236x/cd/da/4b/cdda4beceee4594f5bdc0cf505a73d8b--camille-lol-league-of-legends-camille.jpg',
+  size: '40px',
+  radius: '100px',
+  status: 'offline'
+};
+Avatar.propTypes = {
+  src: _propTypes["default"].string,
+  size: _propTypes["default"].string,
+  radius: _propTypes["default"].string,
+  status: _propTypes["default"].oneOf(['online', 'away', 'offline'])
+};
--- src/Avatar.js
+++ src/Avatar.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Avatar = ({ 
-    src = 'https://i.pinimg.com/236x/cd/da/4b/cdda4beceee4594f5bdc0cf505a73d8b--camille-lol-league-of-legends-camille.jpg',
-    width = '32px', 
-    height = '32px',
-    borderRadius = '100px',
-    status = 'offline',
-    ...rest 
-}) => {
+export const Avatar = ({ src, size, radius, status, ...rest }) => {
     return (
-        <img src={src} style={{ width, height, borderRadius, objectFit: 'cover' }} {...rest} />
-    )
+        <img src={src} style={{ width: size, height: size, borderRadius: radius, objectFit: 'cover' }} {...rest} />
+    );
+};
+
+Avatar.defaultProps = {
+    src: 'https://i.pinimg.com/236x/cd/da/4b/cdda4beceee4594f5bdc0cf505a73d8b--camille-lol-league-of-legends-camille.jpg',
+    size: '40px', 
+    radius: '100px',
+    status: 'offline'
+};
+
+Avatar.propTypes = {
+    src: PropTypes.string,
+    size: PropTypes.string,
+    radius: PropTypes.string,
+    status: PropTypes.oneOf(['online', 'away', 'offline'])
 };
